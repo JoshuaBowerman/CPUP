2b717f789feaf642bf5f1c161db13fd6dc0086c6
Joshua Bowerman <JoshuaBowerman@users.noreply.github.com>
2021-01-20 15:01:13 -0500

    ALU Complete
    The ALU has been completed and tested

--- DE0_NANO/CPUP/ALU_Controller.v
+++ DE0_NANO/CPUP/ALU_Controller.v
@@ -2,12 +2,97 @@
 
 module ALU_Controller(
 inout		[15:0]	bus,
-input		[7:0]		ALU_control_bus,
-input 				clock
-);
+input		[8:0]		ALU_control_bus,
+input 				clock,
+output	[15:0]	A_out,
+output	[15:0]	B_out,
+output				dclken,
+output				daclr,
+input		[15:0]	divq,
+input		[15:0]	divr
+); 
 
-reg oe = 0;
+reg [16:0]  A;
+reg [16:0]	B;
+reg [16:0]	Result;
+reg [15:0]	Error;
+
+wire [16:0] add;
+wire [16:0] sub;
+wire [16:0] mul;
+
+
+assign add = A + bus;
+assign sub = A - bus;
+assign mul = A * bus;
+
+wire oe;
+assign oe = ALU_control_bus[1] | ALU_control_bus[6];
 reg [15:0] bus_out;
 assign bus = oe ? bus_out : 16'bZ;
+
+
+wire we;
+assign we = |ALU_control_bus[5:2] | ALU_control_bus[8];
+
+assign A_out = A[15:0];
+assign B_out = B[15:0];
+
+//Output
+always @(posedge oe or posedge clock)
+	begin
+	if(oe)
+		begin
+			if(ALU_control_bus[1])
+			bus_out <= Result[15:0];
+		else if (ALU_control_bus[6])
+			bus_out <= Error;
+		end
+	end
+	
+	
+//Input and other negative edge things
+always @(negedge clock)
+	begin
+	if(ALU_control_bus[0])
+		A[15:0] <= bus;
+	else if(ALU_control_bus[7])
+		Error <= bus;
+		
+	if(we)
+		B[15:0] <= bus;
+		
+	//Add
+	if(ALU_control_bus[2])
+		begin
+		Result <= add[15:0];
+		Error[0] <= add[16];
+		end
+	//Subtract
+	if(ALU_control_bus[3])
+		begin
+		Result <= sub[15:0];
+		Error[0] <= sub[16];
+		end
+	//Multiplication	
+	if(ALU_control_bus[4])
+		begin
+		Result <= mul[15:0];
+		Error[0] <= mul[16];
+		end
+	//Division	
+	if(ALU_control_bus[5])
+		begin
+		Result <= divq;
+		end
+	//Modulus	
+	if(ALU_control_bus[8])
+		begin
+		Result <= divr;
+		end
+	end
+	
+
+
 
 endmodule 
--- DE0_NANO/CPUP/Execution_Module.v
+++ DE0_NANO/CPUP/Execution_Module.v
@@ -5,7 +5,7 @@ input 				clock,
 input					d_inc,
 output	[11:0]	RCB,
 output	[3:0]		MCB,
-output	[7:0]		ACB,
+output	[8:0]		ACB,
 output	[2:0]		ICB,
 input					paging,
 input		[15:0]	instruction
@@ -15,5 +15,16 @@ input		[15:0]	instruction
 reg oe = 0;
 reg [15:0] bus_out;
 assign bus = oe ? bus_out : 16'bZ;
+
+
+//Temp
+assign RCB = 12'bz;
+assign MCB = 4'bz;
+assign ACB = 8'bz;
+assign ICB = 3'bz;
+
+
+
+
 
 endmodule 
