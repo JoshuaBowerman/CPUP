098dbca76b5dc131d37384ce48dc1c0413ba5b5e
Joshua Bowerman <JoshuaBowerman@users.noreply.github.com>
2021-01-21 14:33:27 -0500

    Memory module now works
    still need to make the instruction register work

--- DE0_NANO/CPUP/memory_controller.v
+++ DE0_NANO/CPUP/memory_controller.v
@@ -9,11 +9,8 @@ inout  [15:0]	bus,
 input  [15:0]	address_space,
 input  [3:0]	mem_control_bus,
 input				clock,
-output [15:0]  instruction_bus,
-output [5:0]   instruction_address,
-output			inst_cache_we,
-output 			paging,
-output [15:0]	inst_offset,
+output [15:0]  instruction,
+output 			double_increment,
 input	 [15:0]	P_reg
 );
 
@@ -23,10 +20,13 @@ input	 [15:0]	P_reg
 
 assign bus = mem_control_bus[2] ? dram_dq : 16'bZ;
 assign dram_data = mem_control_bus[1] ? bus : 16'b0;
+assign we = mem_control_bus[1];
+
+
 
 reg [15:0] addr;
 assign dram_addr = addr[14:0];
-
+assign instruction_bus = addr;
 always @(negedge clock)
 	begin
 	if(mem_control_bus[0])
