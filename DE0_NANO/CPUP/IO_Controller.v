1b44a3379d17ec94cb277b94c45afa3806524eab
JoshuaBowerman <JoshuaBowerman@users.noreply.github.com>
2021-02-19 14:00:04 -0500

    Redesigning Serial
    Changed serial io to use a predefined module for rs232. saves me some time

--- DE0_NANO/CPUP/IO_Controller.v
+++ DE0_NANO/CPUP/IO_Controller.v
@@ -4,11 +4,20 @@ module	IO_Controller(
 inout		[15:0]	bus,
 input		[2:0]		SerialController,
 input		[7:0]		SerialData_in,
-input    			SerialEmpty,
-output				SerialRead,
 output	[7:0]		SerialData_out,
-output				SerialFull,
-output				SerialWrite,
+input					SerialDataInValid,
+output				SerialDataOutValid,
+input					SerialDataError,
+output				SerialInReady,
+input					SerialOutReady,
+output				SerialReset,
+output				SerialMemwrreq,
+output				SerialMemrdreq,
+input					SerialMemEmpty,
+input					SerialMemFull,
+output	[7:0]		SerialMemOut,
+input		[7:0]		SerialMemIn,
+
 input 				clock
 );
 
@@ -32,13 +41,11 @@ assign SerialRead = serialR;
 always @(posedge clock)
 begin
 	
-	serialW <= 0;
-	serialR <= 0;
 	if(SerialController[2])
 	begin
 		if(bus == 4'b0010) //Serial Output
 		begin
-			write <= 1;
+			//write <= 1;
 		end
 	end
 end
@@ -49,27 +56,7 @@ begin
 	begin
 		IO <= bus;
 	end
-	if(SerialController[2])
-	begin
-			if(bus == 4'b0000) //Is there serial data
-		begin
-			IO <= SerialEmpty ? 0 : 1;
-		end
-		if(bus == 4'b0001) //Is the output buffer full
-		begin
-			IO <= SerialFull ? 1 : 0;
-		end
-	end
-	if(write)
-	begin
-		write <= 0;
-		serialW <= 1;
-	end
-	if(read)
-	begin
-		read <= 0;
-		serialR <= 1;
-	end
+	
 end
 endmodule 
 
